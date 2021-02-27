f609baf385e0b7c56253a6f2dacde5c979f97d38
Ramzan Ali <76533850+ramzanroni@users.noreply.github.com>
2021-02-27 22:14:39 +0600

    Update dbconn.php

--- admin/dbconn.php
+++ admin/dbconn.php
@@ -1,5 +1,5 @@
 <?php
- $conn =new mysqli("localhost:3306", "ramzan_sa", "ramzan", "ramzan_hotel"); 
+ $conn =new mysqli("localhost", "root", "", "for_hotel"); 
  if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
